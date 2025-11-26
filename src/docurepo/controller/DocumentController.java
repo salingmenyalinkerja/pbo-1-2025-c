f3d36a59f767ff4fb6094dba6f77465c881a68a3
salingmenyalinkerja <salingmenyalinkerja@gmail.com>
2025-11-26 12:53:30 +0700

    add login view

--- src/docurepo/controller/DocumentController.java
+++ src/docurepo/controller/DocumentController.java
@@ -1,5 +1,30 @@
 package docurepo.controller;
 
+import docurepo.model.User;
+import docurepo.view.DocumentView;
+
+import java.util.ArrayList;
+
 public class DocumentController {
-    
+
+    private ArrayList<User> users = new ArrayList<>();
+    private DocumentView view = new DocumentView();
+
+    public DocumentController() {
+        users.add(new User("admin", "admin"));
+        users.add(new User("user", "123"));
+    }
+
+    public boolean login(String username, String password) {
+        for (User u : users) {
+            if (u.getUsername().equals(username) &&
+                u.getPassword().equals(password)) {
+
+                view.showMessage("Login berhasil!");
+                return true;
+            }
+        }
+        view.showMessage("Login gagal!");
+        return false;
+    }
 }
